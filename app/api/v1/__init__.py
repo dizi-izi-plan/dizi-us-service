5ac380a36a3c47b70d1bb9aec306b4bc0fa66b1e
ulyana <ulyanaworkpage@gmail.com>
2026-02-27 15:02:30 +0300

    feat: add yandex endpoints

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -2,11 +2,15 @@ from fastapi import APIRouter
 from app.api.v1.auth import router as auth_router
 from app.api.v1.tariff import router as tariff_router
 from app.api.v1.subscription import router as subscription_router
+from app.api.v1.google import router as google_router
+from app.api.v1.yandex import router as yandex_router
 
 v1_router = APIRouter()
 
 v1_router.include_router(auth_router, prefix="/auth", tags=["Auth V1"])
-v1_router.include_router(tariff_router, prefix="/tariff", tags=["Tariffs V1"])
+v1_router.include_router(yandex_router, prefix="/auth/yandex", tags=["Yandex"])
+v1_router.include_router(google_router, prefix="/auth/google", tags=["Google"])
+v1_router.include_router(tariff_router, prefix="/tariff", tags=["Tariff"])
 v1_router.include_router(
     subscription_router,
     prefix="/subscription",
